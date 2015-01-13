a98fbe68412d0367cce53688434c3b4ae5a56be8
Skarath01 <thegameempire@outlook.com>
2015-01-13 20:49:36 +0100

    km

--- New Unity Project 1/Assets/Scripts/Light_Switch.cs
+++ New Unity Project 1/Assets/Scripts/Light_Switch.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Light_Switch : MonoBehaviour {
 	public Animator anim;
-	void Start(){ anim =gameObject.GetComponent<Animator>();}
+	void Start(){ anim = gameObject.GetComponent<Animator>();}
 	void OnMouseDown () {anim.SetTrigger("Switch_lights");}
-	
+
 }
