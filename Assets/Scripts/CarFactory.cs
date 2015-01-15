3b585f910432295cb3ae55b7e7c3494567b01331
frankrs <frankstapleton3d@gmail.com>
2015-01-15 15:58:29 -0500

    made car factories

---
+++ Assets/Scripts/CarFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarFactory : MonoBehaviour {
+
+	public GameObject[] cars;
+	public float minGenTime;
+	public float maxGenTime;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("BuildCar");
+	}
+
+	public IEnumerator BuildCar (){
+		yield return new WaitForSeconds(Random.Range(minGenTime,maxGenTime));
+		GameObject car = GameObject.Instantiate(cars[0],transform.position,transform.rotation) as GameObject;
+		StartCoroutine("BuildCar");
+	}
+
+}
