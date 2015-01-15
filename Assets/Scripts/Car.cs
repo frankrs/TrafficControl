628dceed93e4509ebbb138af3bc3017b1c58faad
frankrs <frankstapleton3d@gmail.com>
2015-01-15 16:25:47 -0500

    crated option for cars to use wheels

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,10 +3,27 @@ using System.Collections;
 
 public class Car : MonoBehaviour {
 
+	public bool useWheels = false;
+
 
 	public float speed;
 
+	public float wheelTorque = 30f;
+
+	public WheelCollider[] wheels;
+
+	void Awake(){
+		wheels = transform.GetComponentsInChildren<WheelCollider>();
+	}
+
 	void FixedUpdate () {
+		if(useWheels){
+			foreach(WheelCollider w in wheels){
+				w.motorTorque = wheelTorque;
+			}
+		}
+		else{
 		rigidbody.AddRelativeForce(Vector3.forward * speed);
+		}
 	}
 }
