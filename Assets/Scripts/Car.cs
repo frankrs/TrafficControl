f6f041ea9469006081724f1c8a1891312e78c5b6
frankrs <frankstapleton3d@gmail.com>
2015-01-15 15:37:42 -0500

    messed with car

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+
+	public float speed;
+
+	void FixedUpdate () {
+		rigidbody.AddRelativeForce(Vector3.forward * speed);
+	}
+}
