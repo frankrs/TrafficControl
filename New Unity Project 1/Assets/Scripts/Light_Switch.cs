9900014523eb55b2d11c9e7b91b3063a1a8c7d60
Skarath01 <thegameempire@outlook.com>
2015-01-15 20:51:58 +0100

    k

--- New Unity Project 1/Assets/Scripts/Light_Switch.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Light_Switch : MonoBehaviour {
-	public Animator anim;
-	void Start(){ anim = gameObject.GetComponent<Animator>();}
-	void OnMouseDown () {anim.SetTrigger("Switch_lights");}
-
-}
