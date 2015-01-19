d4f708ca4f5a4351a33cb7466f5d2ab163238985
frankrs <frankstapleton3d@gmail.com>
2015-01-19 16:25:46 -0500

    created new traffic light

--- Assets/Scripts/Light_Switch.cs
+++ Assets/Scripts/Light_Switch.cs
@@ -3,10 +3,38 @@ using System.Collections;
 
 public class Light_Switch : MonoBehaviour {
 	public Animator anim;
-	public Collider stopCollider;
+	public BoxCollider stopCollider;
+	public LightState lightState;
+
+
+	void OnDrawGizmos(){
+	switch(lightState){
+		case LightState.Red:
+		Gizmos.color = new Color(1, 0, 0, 0.5F);
+		break;
+
+		case LightState.Green:
+		Gizmos.color = new Color(0, 1, 0, 0.5F);
+		break;
+	}
+		Gizmos.DrawCube(stopCollider.transform.position + stopCollider.center, stopCollider.size);
+		Gizmos.DrawLine(transform.position,stopCollider.transform.position + stopCollider.center);
+	}
+
 
 	void Start(){ 
 		anim = gameObject.GetComponent<Animator>();
+
+		switch(lightState){
+		case LightState.Red:
+			anim.SetTrigger("Red");
+			break;
+			
+		case LightState.Green:
+			anim.SetTrigger("Green");
+			break;
+		}
+
 	}
 
 	void OnMouseDown () {
@@ -15,10 +43,17 @@ public class Light_Switch : MonoBehaviour {
 
 	public void RedLight(){
 		stopCollider.enabled = true;
+		lightState = LightState.Red;
 	}
 
 	public void GreenLight (){
 		stopCollider.enabled = false;
+		lightState = LightState.Green;
 	}
 
+}
+
+[System.Serializable]
+public enum LightState{
+	Red,Green
 }
