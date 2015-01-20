9f4ad9922551e797097009a481d3222d3aaea83f
frankrs <frankstapleton3d@gmail.com>
2015-01-20 14:32:18 -0500

    created traffic jam

--- Assets/Scripts/CarFactory.cs
+++ Assets/Scripts/CarFactory.cs
@@ -6,6 +6,11 @@ public class CarFactory : MonoBehaviour {
 	public GameObject[] cars;
 	public float minGenTime;
 	public float maxGenTime;
+	public LayerMask carLayer;
+	public float proxiomity = 1f;
+
+	public delegate void Jam();
+	public static event Jam OnJam;
 
 	// Use this for initialization
 	void Start () {
@@ -14,8 +19,15 @@ public class CarFactory : MonoBehaviour {
 
 	public IEnumerator BuildCar (){
 		yield return new WaitForSeconds(Random.Range(minGenTime,maxGenTime));
-		GameObject car = GameObject.Instantiate(cars[0],transform.position,transform.rotation) as GameObject;
-		StartCoroutine("BuildCar");
+		RaycastHit hit;
+		if(Physics.SphereCast((new Vector3(transform.position.x,transform.position.y + 10f,transform.position.z)),proxiomity,-transform.up, out hit,15f, carLayer)){
+			OnJam();
+			StartCoroutine("BuildCar");
+		}
+		else{
+			GameObject car = GameObject.Instantiate(cars[0],transform.position,transform.rotation) as GameObject;
+			StartCoroutine("BuildCar");
+		}
 	}
 
 }
