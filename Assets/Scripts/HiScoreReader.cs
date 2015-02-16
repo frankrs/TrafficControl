87d26dd2face3b63e1264edfbb2a368d03d65b82
frankrs <frankstapleton3d@gmail.com>
2015-02-15 19:53:25 -0500

    main Menu shit

---
+++ Assets/Scripts/HiScoreReader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HiScoreReader : MonoBehaviour {
+
+	public Text hiScoreText;
+
+
+	void Start () {
+		if(PlayerPrefs.HasKey("HiScore")){
+			hiScoreText.text = PlayerPrefs.GetInt("HiScore").ToString();
+		}
+		else{
+			hiScoreText.text = "NO SCORES SET";
+		}
+	}
+	
+
+}
