8eb446eea11512532cb3dba98022f731c22d2d49
frankrs <frankstapleton3d@gmail.com>
2015-02-21 09:30:02 -0500

    small changes

--- Assets/Scripts/Wrecker.cs
+++ Assets/Scripts/Wrecker.cs
@@ -4,13 +4,22 @@ using System.Collections;
 public class Wrecker : MonoBehaviour {
 
 	public LayerMask carLayer;
+
+	//private 
 	
 	void Update () {
+
+//#if UNITY_ANDROID
+
+//#else
 		if(Input.GetMouseButtonDown(0)){
 			RaycastHit hit;
 			if(Physics.Raycast(Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0f)),out hit,Mathf.Infinity,carLayer)){
 				hit.collider.SendMessageUpwards("OnWrecker");
 			 }
 		}
+
+//#endif
+
 	}
 }
