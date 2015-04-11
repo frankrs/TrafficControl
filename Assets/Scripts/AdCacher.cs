eedbf51ee31b4fcfc54640f1427ccde835285075
frankrs <frankstapleton3d@gmail.com>
2015-04-11 12:20:48 -0400

    latest published version

---
+++ Assets/Scripts/AdCacher.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using ChartboostSDK;
+
+public class AdCacher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Chartboost.cacheInterstitial(CBLocation.Default);
+	}
+	
+
+}
