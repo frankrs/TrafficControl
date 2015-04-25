4e2d0f1d327bbe9f100c2c67c604a8b09c6ea9c8
frankrs <frankstapleton3d@gmail.com>
2015-04-25 02:58:58 -0400

    added achievements pannel

---
+++ Assets/Scripts/Franks_FB_Test.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook;
+using System.Collections.Generic;
+using System.Linq;
+
+using Facebook;
+using Facebook.MiniJSON;
+
+public class Franks_FB_Test : MonoBehaviour {
+
+
+	public void FBInit () {
+		FB.Init(onInitComplete);
+		//FB.Init( InitDelegate onInitComplete, HideUnityDelegate hideUnityDelegate = null,string authResponse = null);
+	}
+	
+
+	public void FBLog () {
+		FB.Login("email,publish_actions",null);
+	}
+
+
+	public void FBFeed (){
+		FB.Feed();
+	}
+
+
+	public void FBReq(){
+		FB.AppRequest(message: "Roger", title: "Is this Working");
+	}
+
+	private void onInitComplete (){
+		Camera.main.backgroundColor = Color.red;
+	}
+
+
+
+	
+
+}
